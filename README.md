2d0bfbc4b82068f9a14874ce2a497cc5bd790bc6
Anibal Copitan <github@pprios.com>
2024-05-23 13:55:40 -0400

    [add] project ready to simple scrap with docs

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 
 # Simple whatsapp scrap
 
-python
-selenium
+This is the simple way. You must install one by one
 
+    chromium browser [Install](https://www.chromium.org/getting-involved/download-chromium/)
+    python 3.12
 
-# install
+# [For Project Maker] : install and config a python project
+
 Create virtual enviroment
 
     python -m venv venv
     source venv/bin/activate
+    # pip freeze > requirements.txt # Optional. usefull command to print the packages used by project
+
+install selenium and requests pip packages
+
+    pip install selenium
+    pip install requests
+
+# [For Developer] : Easy get started
+
+    cd <your-project-directory>
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+Execute the python app on the terminal
+
+    python index.py 
 
 
+![project working](./docs/2024-05-23_13-46.png)
 
--- index.py
+++ index.py
@@ -1,28 +1,41 @@
+# 
+# You must be install Chromium browser
+#  (for use the webdriver)
+# 
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import requests
 
 # Inicializar el navegador
-driver = webdriver.Chrome()  # ChromeDriver debe estar en tu PATH
+driver = webdriver.Chrome()  # ChromeDriver debe estar en tu PATH (OS)
 driver.get("https://web.whatsapp.com/")
-time.sleep(15)  # Esperar tiempo suficiente para escanear el código QR
+time.sleep(20)  # Esperar tiempo suficiente para escanear el código QR
 
 def descargar_foto_de_perfil(nombre_contacto):
-    # Buscar el campo de búsqueda y escribir el nombre del contacto
-    search_box = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="3"]')
+    # 01. Buscar el campo de búsqueda y escribir el nombre del contacto
+    search_box = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]')
     search_box.clear()
     search_box.send_keys(nombre_contacto)
     time.sleep(2)  # Esperar a que aparezcan los resultados de búsqueda
 
-    # Hacer clic en el contacto
-    contact = driver.find_element_by_xpath(f'//span[@title="{nombre_contacto}"]')
+    # 02. Hacer clic en el contacto que se encontro con el nombre ingresado
+    contact = driver.find_element(By.XPATH, '//*[@id="pane-side"]//span[@title="{}"]'.format(nombre_contacto))
     contact.click()
+    print("se clicleo")
     time.sleep(2)  # Esperar a que se abra la conversación con el contacto
 
-    # Obtener la URL de la foto de perfil
-    profile_picture = driver.find_element_by_xpath('//img[@class="_2goTk"]')
+    # 03. Clickear en la foto (para ver la foto mas grande)
+    contact = driver.find_element(By.XPATH, '//*[@id="main"]/header/div[1]')
+    contact.click()
+    print("se clicleo")
+
+    # 04. Obtener la URL de la foto de perfil
+    profile_picture = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[5]/span/div/span/div/div/section/div[1]/div[1]/div/img')
     picture_url = profile_picture.get_attribute("src")
+    print(picture_url)
 
     # Descargar la foto de perfil
     response = requests.get(picture_url)
@@ -31,14 +44,14 @@ def descargar_foto_de_perfil(nombre_contacto):
 
     print(f"Foto de perfil de {nombre_contacto} descargada exitosamente.")
 
+#
+#
+# Started the app
+#
+#
 # Llamar a la función para descargar la foto de perfil de un contacto específico
-contacto_a_buscar = "Juana"
+contacto_a_buscar = "Pepe"
 descargar_foto_de_perfil(contacto_a_buscar)
 
 # Cerrar el navegador
 driver.quit()
-
-
-
-# simple instalar chromiun simple STEP with selenium
-# CHROMIUM BY DEFAULT INSTALL WEBDRIVER INTO THE BINARY FILES
